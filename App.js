0b53a94b1ef8316f825e6727b719680f4b8e8bdb
KietNguyen <kiethayqua@gmail.com>
2021-01-27 10:48:32 +0700

    search destination screen

--- App.js
+++ App.js
@@ -3,13 +3,16 @@ import {Text, View, SafeAreaView, StatusBar} from 'react-native';
 
 import HomeScreen from './src/screens/Home';
 import SearchResultsScreen from './src/screens/SearchResults';
+import DestinationSearchScreen from './src/screens/DestinationSearch';
+
 const App = () => {
   return (
     <>
       <StatusBar barStyle="dark-content" />
       <SafeAreaView>
         {/* <HomeScreen /> */}
-        <SearchResultsScreen />
+        {/* <SearchResultsScreen /> */}
+        <DestinationSearchScreen />
       </SafeAreaView>
     </>
   );
