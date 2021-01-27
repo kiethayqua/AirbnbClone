598ff47346faf35d0b900222038359cfc51ab221
KietNguyen <kiethayqua@gmail.com>
2021-01-27 11:55:58 +0700

    Guests screen

---
+++ src/screens/Guests/styles.js
@@ -0,0 +1,27 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingVertical: 20,
+    borderBottomWidth: 1,
+    borderColor: 'lightgrey',
+    marginHorizontal: 20,
+  },
+  buttonsRow: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  button: {
+    borderWidth: 1,
+    width: 30,
+    height: 30,
+    borderRadius: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderColor: 'lightgrey',
+  },
+});
+
+export default styles;
